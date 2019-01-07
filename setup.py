e48e7e3c9d154ff6a31b8c1b1c6a2966b0ad4aa3
Adrian <al@blackout.ai>
2019-01-07 16:45:22 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='btnexus-node-python',
-      version='3.0.1',
+      version='3.1.0',
       packages = find_packages(),
       py_modules=['btNode'],
       install_requires=[
