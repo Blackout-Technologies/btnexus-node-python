c656635a9d427679ccc407d8a67914b75a1fd5f5
Adrian <al@blackout.ai>
2019-06-24 17:08:06 +0200

    progress in hook

--- btHook.py
+++ btHook.py
@@ -36,7 +36,13 @@ class Hook(Node):
         self.config = json.loads(base64.b64decode(connectHash))
 
         #call super constructor with axon and token set
-        super(Hook, self).__init__(self.config["token"], urlsplit(self.config["host"]).netloc) #strip protocol
+        token = self.config["token"]
+        host = urlsplit(self.config["host"]).netloc
+        if not host:
+            host = self.config["host"] # backwardscompatibility
+        print("HOST: {}".format(host))
+        print("TOKEN: {}".format(token))
+        super(Hook, self).__init__(token, host)
         self.connect()
 
 
@@ -44,10 +50,10 @@ class Hook(Node):
         """
         Setup all Callbacks
         """
+
         # Join complete
         #TODO: does it make sense to put group and topic to the same name? - would suggest group as 'hook' and topic as self.config["id"]
-
-        self.subscribe(self.config["id"], self.config["id"], self.onMessage, 'hookChat')
+        self.subscribe(self.config["id"], 'hookChat', self._onMessage, "onMessage") 
 
         #TODO: why?
         self.subscribe(self.config["id"], self.config["id"], self.state)
@@ -63,7 +69,7 @@ class Hook(Node):
             'state': self.readyState
         })
 
-    def onMessage(self, originalTxt, intent, language, entities, slots, branchName, originalMsg):
+    def _onMessage(self, **kwargs):#originalTxt, intent, language, entities, slots, branchName, originalMsg):
         """
         React on a message forwarded to the hook.
         TODO: what are captions?
@@ -83,16 +89,48 @@ class Hook(Node):
         :param originalMsg: The original message sent from the btNexus (TODO: is it?)
         :type originalMsg: Message(TODO: is it?)
         """
-        pass
+        # print("1. {}".format(originalTxt))
+        # print("2. {}".format(intent))
+        # print("3. {}".format(language))
+        # print("4. {}".format(entities))
+        # print("5. {}".format(slots))
+        # print("6. {}".format(branchName))
+        # print("7. {}".format(originalMsg))
 
-    def say(self, message):
+        self.onMessage(originalTxt=kwargs["text"]["original"], 
+                        intent=kwargs["intent"], 
+                        language=kwargs["language"], 
+                        entities=kwargs["entities"], 
+                        slots=kwargs["slots"], 
+                        branchName=kwargs["branch"]["name"], 
+                        peer=kwargs)
+
+    def onMessage(self, originalTxt, intent, language, entities, slots, branchName, peer):
+        """
+        Overload for your custum hook! - it needs to trigger say
+        """
+        pass
+        self.say(peer, originalTxt + " SELBER!")#TODO:remove!
+
+    def say(self, peer, message):
         """
         publishes the hooks response.
 
         :param originalTxt: the original text
         :type originalTxt: String
         """
-        pass
+        print("say: {}".format(message))
+        peer["message"] = message
+        self.publish(peer["personalityId"], 'chat', 'hookResponse', peer)
+
+    #     peer.info.message = message;
+    #     this.node.publish(peer.info.personalityId, 'chat', {
+    #         hookResponse: peer.info
+    #     });
+    # }
+
+
+
 
     def onReady(self):
         """
