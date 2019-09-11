837f269ddc990fff55ec79ad382e8b30bd605413
Adrian Lubitz <al@blackout.ai>
2019-09-11 16:09:35 +0200

    errors going to stderr

--- nexus/nexusConnector.py
+++ nexus/nexusConnector.py
@@ -308,7 +308,7 @@ class NexusConnector():
         :param error: A Message to send to the error topic
         :type error: String
         """
-        print(error)
+        print(error, file=sys.stderr)
         err = Message("publish")
         err["group"] = "blackout-global"
         err["topic"] = self.errorTopic
