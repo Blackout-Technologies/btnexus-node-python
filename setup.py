dc8b8b4c968ef79d1630781ab7a9e02907072c14
Adrian Lubitz <al@blackout.ai>
2019-09-11 16:16:49 +0200

    vers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='btnexus-node-python',
-      version='3.2.3',
+      version='3.2.4',
       packages = find_packages(),
       py_modules=['btNode', 'btHook', 'btPostRequest'],
       install_requires=[
