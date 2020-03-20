e70591ea6353e0387878a58894193ee9f9c5f733
Adrian Lubitz <al@blackout.ai>
2020-03-20 14:59:59 +0100

    socket.io 4.4.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(name='btnexus-node-python',
           'backports.ssl_match_hostname',
           'requests',
           'python-engineio==3.11.2', # newer version has a bug in 2.7 saying `AttributeError: 'module' object has no attribute 'main_thread'\n threading.current_thread() == threading.main_thread():`
-          'python-socketio', 
+          'python-socketio==4.4.0', 
           'Twisted',
           'pyOpenSSL', 
           'service_identity'
